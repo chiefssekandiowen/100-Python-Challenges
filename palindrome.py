dbce945ee3da9ffaa8abffd5a3e534ae01ec52b8
Chief Ssekandi Owen <chiefssekandiowen@gmail.com>
2025-10-16 18:13:05 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,10 +2,10 @@ word = input("Enter any word:...  ")
 
 def para(text):
     
-    n = list(text)
-    m = list(text)
+    n = list("".join(text.lower().split()))
+    m = list("".join(text.lower().split()))
     m.reverse()
- 
+
     newword = []
 
     for i in range(0, len(m)):
@@ -14,12 +14,11 @@ def para(text):
 
         elif i != 0 and n[i] != m[i]:
             newword.append("-")
-            
+
         else:
             print("Has no match")
 
     return "".join(newword)
 
 print(para(word))
-
 
