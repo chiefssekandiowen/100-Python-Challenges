9dcb7c886062b2e244e7fd3e6f029b6effe969a6
Chief Ssekandi Owen <chiefssekandiowen@gmail.com>
2025-10-16 17:45:31 +0300

    palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -11,16 +11,15 @@ def para(text):
     for i in range(0, len(m)):
         if n[i] == m[i]:
             newword.append(n[i])
-            i += 1
 
         elif i != 0 and n[i] != m[i]:
             newword.append("-")
-            i +=1
-
+            
         else:
             print("Has no match")
 
     return "".join(newword)
 
 print(para(word))
+
 
